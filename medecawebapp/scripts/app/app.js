c8d7e8ccdcd95658e5d6d57773f1bc3d3902032f
bernadevteam <bernaovi91@gmail.com>
2016-04-08 11:23:52 -0400

    V1.0.3

--- medecawebapp/scripts/app/app.js
+++ medecawebapp/scripts/app/app.js
@@ -1,85 +1,6 @@
 ﻿'use strict';
 
-
-
 angular.module('medecaApp', ['ngOrderObjectBy', 'checklist-model', 'ngMaterial', 'angular.morris-chart', 'mwl.calendar', 'ui.bootstrap', 'angularUtils.directives.dirPagination'])
-    .directive('ngUnique', ['$http', function ($http) {
-        return {
-            require: 'ngModel',
-            link: function (scope, elem, attrs, ctrl) {
-                elem.on('blur', function (evt) {
-                    ctrl.$setValidity('unique', true);
-                    scope.uniquemsg = '';
-                    var thisVal = elem.val();
-
-                    if (ctrl.$$parentForm[elem.attr('name')].$dirty && ctrl.$$parentForm[elem.attr('name')].$valid)
-                    scope.$apply(function () {
-                        $http({ 
-                            method: 'GET', 
-                            url: attrs.ngUnique + '?field=' + thisVal + '&id=' + (attrs.ngModid || 0)
-                        }).success(function(data, status, headers, config) {
-                            ctrl.$setValidity('unique', data.Existe == 0);
-                            scope.uniquemsg = data.Nombre;
-                        });
-                    });
-                })
-                }
-        }}
-    ])
-     .directive('manageVehiculo', function () {
-         return {
-             restrict: 'E',
-             scope: false,
-             templateUrl: '/partials/manageVehiculo.html'
-         }
-     })
-       .directive('manageClient', function () {
-           return {
-               restrict: 'E',
-               scope: false,
-               templateUrl: '/partials/manageClient.html'
-           }
-       })
-          .directive('viewClient', function () {
-              return {
-                  restrict: 'E',
-                  scope: false,
-                  templateUrl: '/partials/viewClient.html'
-              }
-          })
-     .directive('viewVehicle', function () {
-         return {
-             restrict: 'E',
-             scope: false,
-             templateUrl: '/partials/viewVehicle.html'
-         }
-     })
-       .directive('viewOrder', function () {
-           return {
-               restrict: 'E',
-               scope: false,
-               templateUrl: '/partials/viewOrder.html'
-           }
-       })
-    .directive('loading', ['$http', function ($http) {
-    return {
-        restrict: 'A',
-        link: function (scope, elm, attrs) {
-            scope.isLoading = function () {
-                return $http.pendingRequests.length > 0;
-            };
-
-            scope.$watch(scope.isLoading, function (v) {
-                if (v) {
-                    elm.show();
-                } else {
-                    elm.hide();
-                }
-            });
-        }
-    };
-
-    }])
 .run(function ($rootScope) {
     /*
         Receive emitted message and broadcast it.
@@ -105,3 +26,17 @@ angular.module('medecaApp', ['ngOrderObjectBy', 'checklist-model', 'ngMaterial',
         $rootScope.$broadcast('manageeditingclient', args);
     });
 });;
+
+function switchModal(el, modalTosSwitch) {
+    var parentModal = $(el).closest(".modal");
+    parentModal.modal('hide');
+    parentModal.on('hidden.bs.modal', function (e) {
+        $(modalTosSwitch).modal({
+            keyboard: false,
+            backdrop: 'static'
+        });
+        parentModal.unbind('hidden.bs.modal');
+
+    });
+
+}
---
+++ medecawebapp/scripts/app/directives.js
@@ -0,0 +1,102 @@
+﻿'use strict';
+ 
+angular.module('medecaApp')
+  .directive('ngUnique', ['$http', function ($http) {
+      return {
+          require: 'ngModel',
+          link: function (scope, elem, attrs, ctrl) {
+              elem.on('blur', function (evt) {
+                  ctrl.$setValidity('unique', true);
+                  scope.uniquemsg = '';
+                  var thisVal = elem.val();
+
+                  if (ctrl.$$parentForm[elem.attr('name')].$dirty && ctrl.$$parentForm[elem.attr('name')].$valid)
+                      scope.$apply(function () {
+                          $http({
+                              method: 'GET',
+                              url: attrs.ngUnique + '?field=' + thisVal + '&id=' + (attrs.ngModid || 0)
+                          }).success(function (data, status, headers, config) {
+                              ctrl.$setValidity('unique', data.Existe == 0);
+                              scope.uniquemsg = data.Nombre;
+                          });
+                      });
+              })
+          }
+      }
+  }
+  ])
+     .directive('manageVehiculo', function () {
+         return {
+             restrict: 'E',
+             scope: false,
+             templateUrl: '/partials/manageVehiculo.html?V1.0.3'
+         }
+     })
+       .directive('manageClient', function () {
+           return {
+               restrict: 'E',
+               scope: false,
+               templateUrl: '/partials/manageClient.html?V1.0.3'
+           }
+       })
+          .directive('viewClient', function () {
+              return {
+                  restrict: 'E',
+                  scope: false,
+                  templateUrl: '/partials/viewClient.html?V1.0.3'
+              }
+          })
+     .directive('viewVehicle', function () {
+         return {
+             restrict: 'E',
+             scope: false,
+             templateUrl: '/partials/viewVehicle.html?V1.0.3'
+         }
+     })
+       .directive('viewOrder', function () {
+           return {
+               restrict: 'E',
+               scope: false,
+               templateUrl: '/partials/viewOrder.html?V1.0.3'
+           }
+       })
+         .directive('manageModel', function () {
+             return {
+                 restrict: 'E',
+                 scope: false,
+                 templateUrl: '/partials/manageModel.html?V1.0.3'
+             }
+         })
+      .directive('manageProvider', function () {
+          return {
+              restrict: 'E',
+              scope: false,
+              templateUrl: '/partials/manageProviders.html?V1.0.3'
+          }
+      })
+          .directive('manageServices', function () {
+              return {
+                  restrict: 'E',
+                  scope: false,
+                  templateUrl: '/partials/manageServices.html?V1.0.3'
+              }
+          })
+    .directive('loading', ['$http', function ($http) {
+        return {
+            restrict: 'A',
+            link: function (scope, elm, attrs) {
+                scope.isLoading = function () {
+                    return $http.pendingRequests.length > 0;
+                };
+
+                scope.$watch(scope.isLoading, function (v) {
+                    if (v) {
+                        elm.show();
+                    } else {
+                        elm.hide();
+                    }
+                });
+            }
+        };
+
+    }]);
